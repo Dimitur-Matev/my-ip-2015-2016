9edfd569c074343c1ee4bc4254ce49e8f5aa7c27
BRANIAC <dimmat97@gmail.com>
2015-11-14 17:33:33 +0200

    Added handling for multiple clients

--- ip_availability/src/ip_availability/ClientHandler.java
+++ ip_availability/src/ip_availability/ClientHandler.java
@@ -9,24 +9,27 @@ public class ClientHandler {
 	private static final String COMMAND_STOP_SERVER = "stopServer";
 
 	private final Socket socket;
+	private final Server server;
 
-	public ClientHandler(Socket socket) {
+	public ClientHandler(Server server, Socket socket) {
 		this.socket = socket;
+		this.server = server;
 	}
-	
+
 	public void run() {
 		try {
-			final PrintStream out = 
-				new PrintStream(socket.getOutputStream());
-			final Scanner scanner =
-				new Scanner(socket.getInputStream());
+			final PrintStream out = new PrintStream(socket.getOutputStream());
+			final Scanner scanner = new Scanner(socket.getInputStream());
+
 			while (scanner.hasNextLine()) {
 				final String line = scanner.nextLine();
 				if (COMMAND_STOP_SERVER.equals(line)) {
+					server.stopServer();
 					break;
 				}
 				out.println(line);
 			}
+
 			scanner.close();
 			out.close();
 		} catch (IOException e) {
--- ip_availability/src/ip_availability/Server.java
+++ ip_availability/src/ip_availability/Server.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class Server {
 	private final int port;
+	private boolean running = true;
 
 	public Server(int port) {
 		this.port = port;
@@ -16,10 +17,16 @@ public class Server {
 	public void startServer() throws IOException {
 
 		final ServerSocket serverSocket = new ServerSocket(port);
-		final Socket socket = serverSocket.accept();
-		final ClientHandler client = new ClientHandler(socket);
-		client.run();
+		while (running) {
+			final Socket socket = serverSocket.accept();
+			final ClientHandler client = new ClientHandler(this ,socket);
+			client.run();
+		}
 		serverSocket.close();
 
+	}
+	
+	public void stopServer(){
+		running = false;
 	}
 }
