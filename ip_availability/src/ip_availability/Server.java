939ac08788d41d27df1b5866672920fa1ce91a70
BRANIAC <dimmat97@gmail.com>
2015-11-14 16:35:31 +0200

    Added server

---
+++ ip_availability/src/ip_availability/Server.java
@@ -0,0 +1,13 @@
+package ip_availability;
+
+public class Server {
+	private final int port;
+
+	public Server(int port) {
+		this.port = port;
+	}
+
+	public void startServer() {
+
+	}
+}
---
+++ ip_availability/src/ip_availability/ServerStarter.java
@@ -0,0 +1,11 @@
+package ip_availability;
+import java.io.IOException;
+
+public class ServerStarter {
+	private static final int SERVER_PORT = 8086;
+
+	public static void main(String[] args) throws IOException {
+		final Server server = new Server(SERVER_PORT);
+		server.startServer();
+	}
+}
